10607716b7c51bedff43c0eb468f59328488a0b9
mehul4804 <mehul.ghosh4804@gmail.com>
2017-01-04 18:27:43 +0530

    Add files via upload

---
+++ Xpattern.java
@@ -0,0 +1,25 @@
+ class Xpattern
+ {
+public static void main(String[] args)
+    {
+        for (int i = 1; i<=5; i++)
+        {
+            for (int j=1; j<=5; j++)
+            {
+                if (i == j)
+                  System.out.print("*");
+                if (i == 1 && j == 5)
+                  System.out.print("*");
+                if (i == 2 && j == 4)
+                 System.out.print("*");
+                if ( i == 4 && j == 2)
+                 System.out.print("*");
+                if (i == 5 && j == 1)
+                 System.out.print("*");
+                else
+                  System.out.print("__"); 
+                     
+            }System.out.println(); 
+        }
+	}
+ }
